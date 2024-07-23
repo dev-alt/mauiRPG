da06d55033e3b10c892aad8eabf712fa15fd4079
Andrew logan <53392022+dev-alt@users.noreply.github.com>
2024-07-23 18:55:57 +1200

    Added enemy base class

--- mauiRPG/Models/Enemy.cs
+++ mauiRPG/Models/Enemy.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace mauiRPG.Models
 {
-    internal class Enemy
+    internal abstract class Enemy
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public double Health { get; set; }  
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required double Health { get; set; }
+        public int MaxHealth { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int Level { get; set; }
+        public int ExperienceValue { get; set; }
+        protected Enemy(string name, int health, int attack, int defense, int level, int experienceValue)
+        {
+            Name = name;
+            MaxHealth = health;
+            Health = MaxHealth;
+            Attack = attack;
+            Defense = defense;
+            Level = level;
+            ExperienceValue = experienceValue;
+        }
+
+    }
+
+    internal class EnemyWizard : Enemy
+    {
+        public EnemyWizard(string name, int level) : base(name, 50 + (level * 10), 5 + (level * 2), 2 + level, level, 10 + (level * 5))
+        {
+        }
+
     }
 }
