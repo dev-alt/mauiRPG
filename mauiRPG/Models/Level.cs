7f0af6d6a7047c00d5b6425642ffbd0394a21a70
Andrew logan <53392022+dev-alt@users.noreply.github.com>
2024-07-28 12:15:47 +1200

    added ViewModel

---
+++ mauiRPG/Models/Level.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mauiRPG.Models
+{
+    public class Level
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public bool IsUnlocked { get; set; }
+        public string ImageSource { get; set; }
+    }
+}
---
+++ mauiRPG/ViewModels/StageSelectViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+using mauiRPG.Models;
+
+namespace mauiRPG.ViewModels;
+
+public class StageSelectViewModel
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private ObservableCollection<Level> _levels;
+
+    public ObservableCollection<Level> Levels
+    {
+        get => _levels;
+        set
+        {
+            _levels = value;
+            OnPropertyChanged(nameof(Levels));
+        }
+    }
+    public ICommand SelectLevelCommand { get; private set; }
+
+    public StageSelectViewModel()
+    {
+        InitializeLevels();
+        SelectLevelCommand = new Command<Level>(OnLevelSelected);
+    }
+
+    private void InitializeLevels()
+    {
+        Levels = new ObservableCollection<Level>
+        {
+            new Level { Number = 1, Name = "The Beginning", IsUnlocked = true, ImageSource = "level1.png" },
+            new Level { Number = 2, Name = "Dark Forest", IsUnlocked = true, ImageSource = "level2.png" },
+            new Level { Number = 3, Name = "Mystic Mountains", IsUnlocked = false, ImageSource = "level3.png" },
+            // Add more levels as needed
+        };
+    }
+
+    private void OnLevelSelected(Level level)
+    {
+        if (level.IsUnlocked)
+        {
+            // Navigate to the selected level
+            Console.WriteLine($"Starting Level {level.Number}: {level.Name}");
+        }
+        else
+        {
+            Console.WriteLine($"Level {level.Number} is locked!");
+        }
+    }
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
